543afd57ad7be9c953543c5ed9bb1173eb8e36ee
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-09-19 10:34:59 -0300

    Update Disnet.py

--- Disnet.py
+++ Disnet.py
@@ -13,21 +13,32 @@ try:
   os.system('clear')
   print(Colorate.Vertical(Colors.red_to_yellow, banners.banner1))
   print(Colorate.Vertical(Colors.red_to_yellow, '\n=> [C] Criado pôr: Phant0m The Great'))
-  print(Colorate.Vertical(Colors.red_to_yellow, '=> [V] v4.0\n'))
-  console.print(Panel.fit("""[red][01] [white]Consultar Token\n\n[red][02] [white]Bug Report\n\n[red][03] [white]Sair""", padding=(1,13), title="[bold yellow]Disnet"))
-  m=input(f'{c.bred}[>>]: {c.white}')
-  if m=='1' or m=='01':
+  print(Colorate.Vertical(Colors.red_to_yellow, '=> [V] v5.0\n'))
+  console.print(Panel.fit("""[red][01] [white]Consultar Token\n\n[red][02] [white]Enviar mensagem em um servidor\n\n[red][03] [white]Bug report\n\n[red][04] [white]Sair""", padding=(1,13), title="[bold yellow]Disnet"))
+  user_response=input(f'{c.bred}[>>]: {c.white}')
+  if user_response=='1' or user_response=='01':
     try:
      util.CT()
     except:
       print(f'\n{c.bred}[ ! ] Oh não ! não foi possível fazer a conexão, tente novamente mais tarde.{c.white}')
       sys.exit()
-  elif m=='2' or m=='02':
+  elif user_response=='2' or user_response=='02':
+    
+      os.system('clear')
+      print(Colorate.Vertical(Colors.red_to_yellow, banners.banner2))
+      token=input(f'\n[{c.red}>{c.white}] Token: ')
+      server_id=input(f'[{c.red}>{c.white}] Id do servidor: ')
+      channel_id=input(f'[{c.red}>{c.white}] Id do canal: ')
+      message=input(f'[{c.red}>{c.white}] Mensagem: ')
+    
+      util.send_message_to_channel(token, server_id, channel_id, message)
+    
+  elif user_response=='3' or user_response=='03':
     os.system('clear')
     print(Colorate.Vertical(Colors.red_to_yellow, banners.banner1))
     print(f'\n[§] Caso tenha encontrado algum bug na ferramenta, entre em contato com o criador para reportar o bug. Discord ↓\n\n[ {c.bblue}D{c.white} ] @phant0mthegreat')
     input('\n[ENTER] para voltar ao menu.')
-  elif m=='3' or m=='03':
+  elif user_response=='4' or user_response=='04':  
     os.system('clear')
     print(Colorate.Vertical(Colors.red_to_yellow, banners.banner1))
     print('\n[#] Até a próxima.')
