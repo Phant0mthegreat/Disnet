a1660d30dc323ee4a07d19db7b9ec3d759c8f4d5
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-09-27 09:12:07 -0300

    Update banners.py

--- banners.py
+++ banners.py
@@ -1,6 +1,12 @@
 import sys, time, cores as c
 from pystyle import Colorate, Colors
 
+texto = "[⚠️] ESTA FERRAMENTA É FEITA SOMENTE PARA FINS EDUCACIONAIS E DE PESQUISA, EU (PHANT0M THE GREAT) NÃO ASSUMO QUALQUER TIPO DE RESPONSABILIDADE POR QUALQUER USO INADEQUADO DESTA FERRAMENTA, USE-A COM BOM SENSO [⚠️]"
+def text():
+  for char in texto:
+      print(char, end='', flush=True)  
+      time.sleep(0.03)
+    
 def Carreg():
 	l = ['|', '-', '|', '-']
 	for i in l+l+l:
@@ -16,14 +22,14 @@ banner0="""
 ──▄████▄──▐▌──▄████▄──"""
 
 banner1="""
-         (                                
-         )\ )                          )  
-         (()/(  (                (   ( /(  
-         /(_)) )\  (    (      ))\  )\()) 
-         (_))_ ((_) )\   )\ )  /((_)(_))/  
-         |   \ (_)((_) _(_/( (_)) | |_/   
-         | |) || |(_-<| ' \))/ -_)|  _|  
-         |___/ |_|/__/|_||_| \___| \__|"""
+            (                                
+            )\ )                          )  
+            (()/(  (                (   ( /(  
+            /(_)) )\  (    (      ))\  )\()) 
+            (_))_ ((_) )\   )\ )  /((_)(_))/  
+            |   \ (_)((_) _(_/( (_)) | |_/   
+            | |) || |(_-<| ' \))/ -_)|  _|  
+            |___/ |_|/__/|_||_| \___| \__|"""
 
 banner2='''               .--.
               /.-. '----------.
